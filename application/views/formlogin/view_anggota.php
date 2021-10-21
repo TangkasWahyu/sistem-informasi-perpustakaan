4ea782b5b98348248319fc30ca8cbf35dca4bd9c
I Made Tangkas Wahyu Kencana Yuda <33147417+TangkasWahyu@users.noreply.github.com>
2021-10-21 11:40:16 +0800

    update commit

--- application/views/formlogin/view_anggota.php
+++ application/views/formlogin/view_anggota.php
@@ -47,7 +47,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="<?php echo site_url('');?>"><strong>PERPUSTAKAAN-DEV</strong></a>
+            <a class="navbar-brand" href="<?php echo site_url('');?>"><strong>Sistem Informasi Perpustakaan</strong></a>
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
